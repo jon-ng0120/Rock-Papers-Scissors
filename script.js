1b6cca56361e89c520f76680739b28987a75c5d4
Jon Ng <jonathanng15@gmail.com>
2021-05-10 22:48:25 -0700

    add score tracking and decide winner

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -13,24 +15,54 @@ function playerPlay() {
 }
 
 function playRound(playerChoice, computerChoice) {
-    let outcome = undefined;
+    let outcome = `Player chose ${playerChoice} and computer chose ${computerChoice}. `;
+
     if (playerChoice == computerChoice) {
-        outcome = "It's a tie"
+        outcome += "It's a tie"
     } else if (playerChoice == 'rock') {
-        outcome = computerChoice == "scissors" ? "Player wins!" : "Computer wins..."
+        if (computerChoice == "scissors") {
+            outcome += "Player wins!";
+            playerScore++;
+        } else {
+            outcome += "Computer wins...";
+            computerScore++
+        }
     } else if (playerChoice == 'paper') {
-        outcome = computerChoice == "rock" ? "Player wins!" : "Computer wins..."
+        if (computerChoice == "rock") {
+            outcome += "Player wins!";
+            playerScore++;
+        } else {
+            outcome += "Computer wins...";
+            computerScore++
+        }
     } else if (playerChoice == 'scissors') {
-        outcome = computerChoice == "paper" ? "Player wins!" : "Computer wins..."
+        if (computerChoice == "paper") {
+            outcome += "Player wins!";
+            playerScore++;
+        } else {
+            outcome += "Computer wins...";
+            computerScore++
+        }
     } 
     return outcome
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
     for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerPlay(), computerPlay()))
+        roundResult = playRound(playerPlay(), computerPlay())
+        console.log(`Round ${i + 1}`)
+        console.log(roundResult)
+
+        console.log(`Player: ${playerScore}`)
+        console.log(`Computer: ${computerScore}`)
+        console.log("-----------------------------------------")
+    }
+    if (playerScore == computerScore) {
+        console.log("It's a tie! No winners")
+    } else if (playerScore > computerScore) {
+        console.log("Player wins it all!")
+    } else {
+        console.log("Computer won it all...")
     }
 }
 
