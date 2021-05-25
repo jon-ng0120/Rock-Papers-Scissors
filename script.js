d4b03935f5ea83e39d5536f48174ccb867751059
Jon Ng <jonathanng15@gmail.com>
2021-05-24 22:33:48 -0700

    Add play again function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // rock, paper, scissor divs
-const roundOutcome = document.querySelector('.round-container');
+const roundOutcome = document.querySelector('.round-text');
 
 const playerContainer = document.querySelector('.player-container');
 const cpuContainer = document.querySelector('.cpu-container');
@@ -10,6 +10,14 @@ const cpuOptions = cpuContainer.querySelectorAll('.option')
 
 let playerScoreContainer = document.querySelector('.player-score')
 let computerScoreContainer = document.querySelector('.cpu-score')
+
+const winnerTextContainer = document.querySelector('.winner-text')
+const gameOverOverlay = document.querySelector('.game-over-container')
+const playAgainBtn = document.querySelector('.play-again')
+
+// hides game over overlay when game starts
+gameOverOverlay.style.display = 'none'; 
+
 
 playerOptions.forEach(option => {
     option.addEventListener('click', function() {
@@ -23,60 +31,53 @@ function computerPlay() {
 }
 
 function playRound(playerChoice, computerChoice) {
-    console.log(playerChoice)
-    console.log(computerChoice)
     if (playerChoice == computerChoice) {
         roundOutcome.innerText = "It's a tie"
     } else if (playerChoice == 'rock') {
         if (computerChoice == "scissors") {
-            roundOutcome.innerText = "Player wins!";
+            roundOutcome.innerText = "Player crushes computer's scissors!";
             playerScoreContainer.innerText++
         } else {
-            roundOutcome.innerText = "Computer wins...";
+            roundOutcome.innerText = "Computer wraps up player's rock...";
             computerScoreContainer.innerText++
         }
     } else if (playerChoice == 'paper') {
         if (computerChoice == "rock") {
-            roundOutcome.innerText= "Player wins!";
+            roundOutcome.innerText = "Player wraps up computer's rock!";
             playerScoreContainer.innerText++
         } else {
-            roundOutcome.innerText = "Computer wins...";
+            roundOutcome.innerText = "Computer cuts up player's paper...";
             computerScoreContainer.innerText++
         }
     } else if (playerChoice == 'scissors') {
         if (computerChoice == "paper") {
-            roundOutcome.innerText = "Player wins!";
+            roundOutcome.innerText = "Player cuts up computer's paper!";
             playerScoreContainer.innerText++
         } else {
-            roundOutcome.innerText = "Computer wins...";
+            roundOutcome.innerText = "Computer crushes player's scissors...";
             computerScoreContainer.innerText++
         }
     } 
-    //return outcome
+    checkWinner()
 }
 
 
 function checkWinner() {
-
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        roundResult = playRound(playerPlay(), computerPlay())
-        console.log(`Round ${i + 1}`)
-        console.log(roundResult)
-
-        console.log(`Player: ${playerScore}`)
-        console.log(`Computer: ${computerScore}`)
-        console.log("-----------------------------------------")
-    }
-    if (playerScore == computerScore) {
-        console.log("It's a tie! No winners")
-    } else if (playerScore > computerScore) {
-        console.log("Player wins it all!")
-    } else {
-        console.log("Computer won it all...")
+    if (playerScoreContainer.innerText == '5') {
+        winnerTextContainer.innerText = "Player Wins!"
+        gameOverOverlay.style.display = 'grid'        
+    } else if (computerScoreContainer.innerText == '5') {
+        winnerTextContainer.innerText = 'Computer wins..'
+        gameOverOverlay.style.display = 'grid'     
     }
 }
+
+playAgainBtn.addEventListener('click', function() {
+    playerScoreContainer.innerText = '0';
+    computerScoreContainer.innerText = '0';
+    roundOutcome.innerText = '';
+    gameOverOverlay.style.display = 'none';
+})
+
 
 // game();
